de0d3e28e3b0e2c32ea6959506325b0ed6d6aae8
kittorapha <35044120+kittorapha@users.noreply.github.com>
2018-08-25 00:26:05 -0300

    Add files via upload

---
+++ Exercicio5/Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double larguraNovo;
+            double comprimentoNovo;
+            double largura;
+            double comprimento;
+            double larguraLocal;
+            double comprimentoLocal;
+            int operacao;
+
+
+
+            Retangulo calculoLocal = new Retangulo();
+
+
+            Console.WriteLine("Digite em cm a largura do piso: ");
+            largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite em cm o comprimento do piso: ");
+            comprimento = int.Parse(Console.ReadLine());
+
+
+
+            calculoLocal.Largura = largura;
+            calculoLocal.Comprimento = comprimento;
+
+
+            Console.WriteLine("1 - Mudar Lados / 2 - Retornar Lados / 3 - Calcular Área / 4 - Calular Perimetro");
+            operacao = int.Parse(Console.ReadLine());
+
+
+                if (operacao == 1)
+                {
+                    Console.WriteLine("Digite em cm a largura do piso: ");
+                    larguraNovo = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite em cm o comprimento do piso: ");
+                    comprimentoNovo = int.Parse(Console.ReadLine());
+
+                    calculoLocal.MudarLados(larguraNovo, comprimentoNovo);
+
+                }
+                else
+                {
+                    if (operacao == 2)
+                    {
+
+                        calculoLocal.RetornarLados(largura, comprimento);
+
+                        Console.WriteLine("Largura: " + calculoLocal.Largura);
+                        Console.WriteLine("Comprimento: " + calculoLocal.Comprimento);
+
+                    }
+                    else
+                    {
+                        if (operacao == 3)
+                        {
+
+                            calculoLocal.CalculoArea(largura, comprimento);
+
+                            Console.WriteLine("Area: " + calculoLocal.Area);
+                        }
+                        else
+                        {
+                            if (operacao == 4)
+                            {
+                                calculoLocal.CalculoPerimetro(largura, comprimento);
+
+                                Console.WriteLine("Perimetro: " + calculoLocal.Perimetro);
+                            }
+
+                        }
+                    }
+                    Console.WriteLine("Digite em cm a largura do local: ");
+                    larguraLocal = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite em cm o comprimento do local: ");
+                    comprimentoLocal = int.Parse(Console.ReadLine());
+
+
+                    calculoLocal.CalculoPiso(larguraLocal, comprimentoLocal);
+                    calculoLocal.CalculoRodape(larguraLocal);
+
+
+                    Console.WriteLine("Pisos a comprar: " + calculoLocal.Piso);
+                    Console.WriteLine("Rodapes a comprar: " + calculoLocal.Rodape);
+
+
+                
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Exercicio5/Exercicio6/Exercicio6/Retangulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    class Retangulo
+    {
+        public double Largura{ get; set; }
+        public double Comprimento{ get; set; }
+        public double Area{ get; set; }
+        public double Perimetro{ get; set; }
+        public double Piso { get; set; }
+        public double Rodape { get; set; }
+
+        public void MudarLados(double larguraNovo, double comprimentoNovo)
+        {
+            this.Largura = larguraNovo;
+            this.Comprimento = comprimentoNovo;
+        }
+
+        public void RetornarLados(double largura, double comprimento)
+        {
+            this.Largura = largura;
+            this.Comprimento = comprimento;
+        }
+
+        public void CalculoArea(double largura, double comprimento)
+        {
+            this.Area = (largura * comprimento);
+        }
+
+        public void CalculoPerimetro(double largura, double comprimento)
+        {
+            this.Perimetro = (largura + comprimento) * 2;
+        }
+
+        public void CalculoPiso(double largura, double comprimento)
+        {
+            this.Piso = Convert.ToInt32((largura * comprimento) / (Largura * Comprimento));
+        }
+
+        public void CalculoRodape(double larguraLocal)
+        {
+            this.Rodape = Convert.ToInt32((2 * larguraLocal) / ((2 * Largura)/100));
+        }
+    }
+}
